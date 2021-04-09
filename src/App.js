e25f3b1c1fadf5da5e0c815be60fddf80c989bce
Burak Özkan <burak.ozkan@dokabilisim.com>
2021-04-09 12:08:31 +0300

    create first chart

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import React, {useState} from "react";
 import './App.css';
+import {Line} from "react-chartjs-2";
 
 function App() {
+  const [data, setData] = useState({
+    labels: ["1","2","3","4","5"],
+    datasets : [
+      {
+        label: "App coding",
+        backgroundColor:"rgba(255,0,255,0.75)",
+        data: [4,5,1,10,32,2,12]
+      },
+      {
+        label: "Subscriptions",
+        backgroundColor:"rgba(0,255,0, 0.75)",
+        data: [14,15,21,0,22,40,35]
+      }
+    ]
+  });
+
+  const setGradientColor = (canvas, color) => {
+    const ctx = canvas.getContext("2d");
+    const gradient = ctx.createLinearGradient(0,0,0, 400);
+    gradient.addColorStop(0, color);
+    gradient.addColorStop(0.95, "rgba(133,255,144,0.85)");
+    return gradient;
+  }
+
+  const getChartData = (canvas) => {
+   if(data.datasets) {
+     let colors = ["rgba(255,0,255,0.75)", "rgba(0,255,0, 0.75)"]
+     data.datasets.forEach((set, i) => {
+         set.backgroundColor = setGradientColor(canvas, colors[i]);
+       set.borderColor = "white";
+       set.borderWidth =  2;
+     })
+   }
+    return data
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="chart-container">
+        <h3>Chart Samples</h3>
+        <Line 
+          options={{
+            responsive:true,
+          }}
+          data={getChartData}
+        />
+      </div>
     </div>
   );
 }
